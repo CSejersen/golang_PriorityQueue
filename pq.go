fd9205399525c1efc50e5daef3a36b443b15ee7d
Christian <cvsejersen@gmail.com>
2024-11-10 17:19:47 +0100

    pq impl

---
+++ pq.go
@@ -0,0 +1,21 @@
+package main
+
+type Node struct {
+	row  int
+	col  int
+	time int
+}
+
+type PriorityQueue []Node
+
+func (pq PriorityQueue) Len() int            { return len(pq) }
+func (pq PriorityQueue) Less(i, j int) bool  { return pq[i].time < pq[j].time }
+func (pq PriorityQueue) Swap(i, j int)       { pq[i], pq[j] = pq[j], pq[i] }
+func (pq *PriorityQueue) Push(x interface{}) { *pq = append(*pq, x.(Node)) }
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	*pq = old[:n-1]
+	return item
+}
